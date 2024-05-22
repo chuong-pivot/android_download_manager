35873f861ab8cadfb10d14987b0a7d0de092f003
chuong-pivot <chuongch@azg.jp>
2024-05-22 14:22:00 +0700

    Update

--- android/src/main/java/com/tk/android_download_manager/DownloadBroadcastReceiver.java
+++ android/src/main/java/com/tk/android_download_manager/DownloadBroadcastReceiver.java
@@ -47,13 +47,13 @@ public class DownloadBroadcastReceiver extends BroadcastReceiver implements Even
     @Override
     public void onListen(Object arguments, EventChannel.EventSink events) {
         this.events = events;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            context.registerReceiver(this, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE), RECEIVER_EXPORTED);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            context.registerReceiver(this, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE), Context.RECEIVER_EXPORTED);
         } else {
             context.registerReceiver(this, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
         }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            context.registerReceiver(this, new IntentFilter(DownloadManager.ACTION_NOTIFICATION_CLICKED), RECEIVER_EXPORTED);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            context.registerReceiver(this, new IntentFilter(DownloadManager.ACTION_NOTIFICATION_CLICKED), Context.RECEIVER_EXPORTED);
         } else {
             context.registerReceiver(this, new IntentFilter(DownloadManager.ACTION_NOTIFICATION_CLICKED));
         }
